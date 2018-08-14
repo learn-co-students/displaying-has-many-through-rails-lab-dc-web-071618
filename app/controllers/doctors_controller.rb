690f62da3a54b8ea05085a0caf2794d02bdf9278
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-08-14 15:21:25 -0400

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,46 @@
+class DoctorsController < ApplicationController
+    def index
+        @doctors = Doctor.all 
+    end
+
+    def new
+        @doctor = Doctor.new
+    end
+
+    def create
+        @doctor = Doctor.new(doctor_params)
+        if @doctor.save 
+            redirect_to doctor_path(@doctor)
+        else
+            render :new
+        end
+    end
+
+    def show 
+        @doctor = Doctor.find(params[:id])
+    end
+
+    def edit 
+        @doctor = Doctor.find(params[:id])
+    end
+    
+    def update
+        @doctor = Doctor.find(params[:id])
+        @doctor.update(doctor_params)
+        if @doctor.save 
+            redirect_to doctor_path(@doctor)
+        else
+            render :edit
+        end
+    end
+
+    def destroy 
+        Doctor.find(params[:id]).destroy 
+        redirect_to doctors_path
+    end
+
+    private 
+    def doctor_params 
+        params.require(:doctor).permit(:name, :department)
+    end
+end
---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,46 @@
+class PatientsController < ApplicationController
+    def index
+        @patients = Patient.all 
+    end
+
+    def new
+        @patient = Patient.new
+    end
+
+    def create
+        @patient = Patient.new(patient_params)
+        if @patient.save 
+            redirect_to patient_path(@patient)
+        else
+            render :new
+        end
+    end
+
+    def show 
+        @patient = Patient.find(params[:id])
+    end
+
+    def edit 
+        @patient = Patient.find(params[:id])
+    end
+    
+    def update
+        @patient = Patient.find(params[:id])
+        @patient.update(patient_params)
+        if @patient.save 
+            redirect_to patient_path(@patient)
+        else
+            render :edit
+        end
+    end
+
+    def destroy 
+        Patient.find(params[:id]).destroy 
+        redirect_to patients_path
+    end
+
+    private 
+    def patient_params 
+        params.require(:patient).permit(:name, :age)
+    end
+end
