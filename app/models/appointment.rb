d88651e98e66232883237fc5bb71f67e28b6f633
github username <github email address>
2018-08-15 11:50:17 -0400

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,17 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+
+  def time
+    "#{appointment_datetime.strftime('%B %d, %Y')} at #{ appointment_datetime.strftime("%R")}"
+  end
+
+  def ptnt_name
+    patient.name
+  end
+
+  def dr_name
+    doctor.name
+  end
+
 end
